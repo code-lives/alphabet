111361d16438b896a304857088e28ece198b6514
lijie1 <1873451007@163.com>
2021-03-08 19:27:26 +0800

    version 1

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# alphabet
+# PinYin
+中文转字母
+
+## 传递参数
+# new 
+$PinYin = new \PinYin\PinYin();
+
+
+## 全部拼音
+$PinYin->convertPinyin("中国人");
+
+zhongguoren
+
+## 全拼用_隔开
+$PinYin->convertPinyinList("中国人");
+
+Array
+(
+    [0] => zhong_guo_ren
+)
+
+
+## 这是首字母
+$PinYin->convertInitalPinyin("中国人");
+
+zgr
+
+## 这是首字母
+$PinYin->convertInitalPinyinList("中国人");
+
+Array
+(
+    [0] => zgr
+)
+## 首字母+全字母用 — 隔开
+$PinYin->convertAllPinyinList('中国人', '_');
+
+Array
+(
+    [full] => Array
+        (
+            [0] => zhong_guo_ren
+        )
+    [inital] => Array
+        (
+            [0] => z_g_r
+        )
+)
---
+++ src/PinYin.php
@@ -0,0 +1,186 @@
+<?php
+namespace PinYin;
+use PinYin\ChinesePinyin;
+
+class PinYin {
+	public static function splitString($string) {
+		$result = array();
+
+		$len = mb_strlen($string);
+		while ($len) {
+			$result[] = mb_substr($string, 0, 1, 'utf8');
+			$string = mb_substr($string, 1, $len, 'utf8');
+			$len = mb_strlen($string);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @desc 单字符一维数组转变成二维拼音数组
+	 * @param array $stringList
+	 * @return array
+	 **/
+	public static function changePinyinList($stringList) {
+		$result = array();
+
+		if (!is_array($stringList)) {
+			return $result;
+		}
+
+		foreach ($stringList as $string) {
+			if ((strlen($string) === 3) && isset(ChinesePinyin::$chinesePinyin[$string])) {
+				// 大部分汉字strlen长度为3，在拼音库里。所有读音都取出。
+				$result[] = ChinesePinyin::$chinesePinyin[$string];
+			} else {
+				$result[] = array($string);
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @desc 将字符串中汉字转换为完整拼音（不支持多音字)，自定义转换连接符，默认为''
+	 * @param string $string
+	 * @param string $separator
+	 * @return string
+	 **/
+	public static function convertPinyin($string, $separator = '') {
+		$result = '';
+
+		if (empty($string)) {
+			return $result;
+		}
+
+		$stringList = self::splitString($string);
+		$pinyinList = self::changePinyinList($stringList);
+
+		$resultList = array();
+		foreach ($pinyinList as $pinyin) {
+			$resultList[] = $pinyin[0];
+		}
+
+		$result = implode($separator, $resultList);
+		return $result;
+	}
+
+	/**
+	 * @desc 将字符串中汉字转换为完整拼音数组（支持所有多音字组合)，自定义转换连接符，默认为''
+	 * @param string $string
+	 * @param string $separator
+	 * @return  array
+	 **/
+	public static function convertPinyinList($string, $separator = '') {
+		$result = array();
+
+		if (empty($string)) {
+			return $result;
+		}
+
+		$stringList = self::splitString($string);
+		$pinyinList = self::changePinyinList($stringList);
+
+		// 弹出二维数组第一个元素作为起始数组。循环读取剩余数组元素。交叉笛卡儿积拼接生成所有多音字情况一维数组。
+		// 将所有可能结果赋值给起始数组，进入下一轮循环。至循环结束。
+		$prevPinyin = array_shift($pinyinList);
+		foreach ($pinyinList as $pinyin) {
+			$tmpPinyinList = array();
+			foreach ($prevPinyin as $strPrevPinyin) {
+				foreach ($pinyin as $strPinyin) {
+					$tmpPinyinList[] = $strPrevPinyin . $separator . $strPinyin;
+				}
+			}
+			$prevPinyin = $tmpPinyinList;
+		}
+		$result = array_unique($prevPinyin);
+		return $result;
+	}
+
+	/**
+	 * @desc 将字符串中汉字转换为首字母拼音（不支持多音字)，自定义转换连接符，默认为''
+	 * @param string $string
+	 * @param string $separator
+	 * @return string
+	 **/
+	public static function convertInitalPinyin($string, $separator = '') {
+		$result = '';
+
+		if (empty($string)) {
+			return $result;
+		}
+
+		$stringList = self::splitString($string);
+		$pinyinList = self::changePinyinList($stringList);
+
+		$resultList = array();
+		foreach ($pinyinList as $pinyin) {
+			if (ord($pinyin[0]) > 129) {
+				// 非 a-z 字母
+				$resultList[] = $pinyin[0];
+			} else {
+				$resultList[] = substr($pinyin[0], 0, 1);
+			}
+		}
+
+		$result = implode($separator, $resultList);
+		return $result;
+	}
+
+	/**
+	 * @desc 将字符串中汉字转换为首字母拼音数组（支持所有多音字组合)，自定义转换连接符，默认为''
+	 * @param string $string
+	 * @param string $separator
+	 * @return  array
+	 **/
+	public static function convertInitalPinyinList($string, $separator = '') {
+		$result = array();
+
+		if (empty($string)) {
+			return $result;
+		}
+
+		$stringList = self::splitString($string);
+		$pinyinList = self::changePinyinList($stringList);
+
+		// 弹出二维数组第一个元素作为起始数组。循环读取剩余数组元素。交叉笛卡儿积拼接生成所有多音字情况一维数组。
+		// 将所有可能结果赋值给起始数组，进入下一轮循环。至循环结束。
+		$prevPinyin = array_shift($pinyinList);
+
+		// 处理起始数组，只有一个汉字和汉字开头的情况
+		foreach ($prevPinyin as $key => $strPrevPinyin) {
+
+			$prevPinyin[$key] = substr($strPrevPinyin, 0, 1);
+
+		}
+
+		foreach ($pinyinList as $pinyin) {
+			$tmpPinyinList = array();
+			foreach ($prevPinyin as $strPrevPinyin) {
+				foreach ($pinyin as $strPinyin) {
+					if (ord($strPinyin) > 129) {
+						// 非 a-z 字母
+						$tmpPinyinList[] = $strPrevPinyin . $separator . $strPinyin;
+					} else {
+						$tmpPinyinList[] = $strPrevPinyin . $separator . substr($strPinyin, 0, 1);
+					}
+				}
+			}
+			$prevPinyin = $tmpPinyinList;
+		}
+		$result = array_unique($prevPinyin);
+		return $result;
+	}
+
+	/**
+	 * @desc 将字符串中汉字转换为完整拼音数组、首字母拼音数组（支持所有多音字组合)，自定义转换连接符，默认为''
+	 * @param string $string
+	 * @param string $separator
+	 * @return  array
+	 **/
+	public static function convertAllPinyinList($string, $separator = '') {
+		$result['full'] = self::convertPinyinList($string, $separator);
+		$result['inital'] = self::convertInitalPinyinList($string, $separator);
+		return $result;
+	}
+}
